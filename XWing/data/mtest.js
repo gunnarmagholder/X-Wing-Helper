35aadfe42c3732705ff10cdf6dc8263f1bc33e45
marvinkieslich <marvin.kieslich@web.de>
2015-03-10 13:40:04 +0100

    mtest.js

---
+++ XWing/data/mtest.js
@@ -0,0 +1,19 @@
+{
+          name: "Hull Upgrade",
+          id: 6,
+          points: 3,
+          modifier_func: function(stats) {
+            return stats.hull += 1;
+          image: "../images/Ausruestung/verbesserte_huelle"
+          }
+        }, {
+          name: "Munitions Failsafe",
+          id: 7,
+          points: 1
+          image: "../images/Ausruestung/ausfallsichere_munition"
+        }, {
+          name: "Stygium Particle Accelerator",
+          id: 8,
+          points: 2,
+          image: "../images/Ausruestung/s_t_beschleuniger.jpg"
+        },
