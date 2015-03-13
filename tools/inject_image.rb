9a12901dc20256ed103934de84797d3c26ce28cc
gunnarmagholder <gmagholder@mac.com>
2015-03-13 20:43:19 +0100

    changed the pathnames to no blanks and concatenating from the mongodb the ship and pilot

--- tools/inject_image.rb
+++ tools/inject_image.rb
@@ -1,9 +1,22 @@
 require 'mongo'
+require 'find' 
 
 db = Mongo::Connection.new("localhost").db("xwing")
 
+
+puts "All collections in the database" 
 db.collection_names.each {
 	|name| puts name
 }
 
+puts "Here are the pilots. All of them" 
 
+coll = db["pilots"]
+puts coll.count()
+
+coll.find().each { |row|
+  if(row["ship"]) 
+    name_to_find = "images/" + row["ship"].downcase.sub(/[\s\.-]/,'')  + "_" +row["name"].downcase.sub(/\s/, '')  
+    puts name_to_find
+  end
+}
