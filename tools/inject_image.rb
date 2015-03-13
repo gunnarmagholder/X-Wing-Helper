63ed2e90a63b31b18a447742d8b9d6c2afa27b0a
gunnarmagholder <gmagholder@mac.com>
2015-03-13 21:10:58 +0100

    changed the pilots card path to one path disregarding faction

--- tools/inject_image.rb
+++ tools/inject_image.rb
@@ -1,5 +1,6 @@
 require 'mongo'
 require 'find' 
+require 'pry'
 
 db = Mongo::Connection.new("localhost").db("xwing")
 
@@ -14,9 +15,18 @@ puts "Here are the pilots. All of them"
 coll = db["pilots"]
 puts coll.count()
 
+pilots_path = []
+Find.find('XWing/images/pilots') do |file|
+  pilots_path << file
+end
+
+puts pilots_path.count
+
 coll.find().each { |row|
   if(row["ship"]) 
-    name_to_find = "images/" + row["ship"].downcase.sub(/[\s\.-]/,'')  + "_" +row["name"].downcase.sub(/\s/, '')  
-    puts name_to_find
+    name_to_find = "XWing/images/pilots/" + row["ship"].downcase.sub(/[\s\.-]/,'')  + "_" +row["name"].downcase.sub(/\s/, '') + ".jpg"
+    if pilots_path.include?(name_to_find)
+      puts "found: " + name_to_find
+    end 
   end
 }
